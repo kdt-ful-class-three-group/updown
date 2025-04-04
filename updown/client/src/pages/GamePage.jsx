acaa88e47751df4efb597d2d15d57949a4951a08
최현준 <choijjunii1@gmail.com>
2025-04-04 09:43:56 +0900

    input만든후 이벤트와 연결 중 콘솔오류

--- updown/client/src/pages/GamePage.jsx
+++ updown/client/src/pages/GamePage.jsx
@@ -1,5 +1,5 @@
 // 리액트, 훅
-import React, { useMemo, useRef } from 'react';
+import React, { useMemo, useRef, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
 // 태그 컴포넌트들
@@ -13,6 +13,8 @@ import { InputNum } from '../utils/InputNum';
 //*  이지,노말,하드 모두 한 템플릿에서 작동
 export function GamePage() {
   const { level } = useParams();
+  const [inputValue, setInputValue] = useState('');
+
 
   const maxNum = useMemo(() => {
     if (level === 'normal') return 50;
@@ -20,23 +22,31 @@ export function GamePage() {
     return 10;
   }, [level]);
 
+  //! input에 입력하면 콘솔이 계속찍힘
   const randomNum = useRef(Math.floor(Math.random() * maxNum) + 1);
   console.log(`Game Start! - ${level} mode`);
   console.log(randomNum.current);
     
-  const handleSubmit = () => {
-    InputNum({
-      value: inputValue,
-      max: maxNum,
-      answer: randomNum.current
-    });
+  const inputHandler = (e) => {
+    setInputValue(e.target.value);
+    console.log(inputValue);
+  }
 
     return (
       <Div>
-        <Heading number={1} content={level} />
-        <Input type='text' />
-        <Button btnName={'입력'} onClick={()=> handleSubmit()} />
+        <Div>
+          <Heading>업다운</Heading>
+          <p>1~{maxNum} 사이의 숫자</p>
+        </Div>
+
+        <Div>
+          <Input value={inputValue} onChange={inputHandler} />
+          <Button 
+            btnName="확인" 
+            onClick={() => InputNum({ value: inputValue, max: maxNum, answer: randomNum.current })} 
+          />
+        </Div>
       </Div>
+      
     );
   }
-}
