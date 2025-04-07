26f4680a0c8cb836462af441bbe9df9ba33043d4
JeonSunil <jeonsunil1225@gmail.com>
2025-04-07 15:01:36 +0900

    성공에 prompt 구현

--- updown/client/src/pages/ResultPage.jsx
+++ updown/client/src/pages/ResultPage.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useState } from 'react';
 import { useLocation,useNavigate } from 'react-router-dom';
 import { Button } from '../components/Button';
 import { Div,Heading } from '../components/Tag';
@@ -14,20 +15,39 @@ export function ResultPage() {
   // location 뒤에 search는 ?status=success 이런식으로 쿼리스트링을 가져옴
   const queryParams = new URLSearchParams(location.search);
   const status = queryParams.get('status'); 
+  const [nickName, setNickName] = useState('');
 
   const successOrFail = status === 'success';
 
   return (
       <Div>
         <Heading number={1} content={successOrFail ? '성공' : '실패'} />
-        <Div>
-          {/* 페이지 이동 버튼들 */}
-          {/* navigate 경로를 '/' 직접설정할 수 있지만 -숫자 이렇게 쓰면 숫자만큼 뒤로 갈 수 있음.
-          서로의 장단점은 아직 모르겠음 */}
+        <Div children={successOrFail ? 
+          <>
           <Button btnName={'다시하기'} onClick={() => navigate(-1)}/>
           <Button btnName={'모드선택'} onClick={() => navigate(-2)}/>
           <Button btnName={'홈'} onClick={() => navigate(-3)}/>
+          <Button btnName={'기록하기'} onClick={() => { 
+              let nickName = prompt("닉네임을 입력하세요", setNickName(''));
+              if(nickName) {
+              console.log(nickName);
+              navigate(-3);
+              }
+          }}/> 
+          </>
+          
+          : 
+          <>
+          <Button btnName={'다시하기'} onClick={() => navigate(-1)}/>
+          <Button btnName={'모드선택'} onClick={() => navigate(-2)}/>
+          <Button btnName={'홈'} onClick={() => navigate(-3)}/>
+          </>
+          } />
+          {/* 페이지 이동 버튼들 */}
+          {/* navigate 경로를 '/' 직접설정할 수 있지만 -숫자 이렇게 쓰면 숫자만큼 뒤로 갈 수 있음.
+          서로의 장단점은 아직 모르겠음 */}
+
         </Div>
-      </Div>
+      // </Div>
   )
 }
