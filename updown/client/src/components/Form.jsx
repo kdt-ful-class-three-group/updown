8803e4c21af25975946ff2d047c0eef2bbba33c3
최현준 <choijjunii1@gmail.com>
2025-04-17 10:00:36 +0900

    Form 컴포넌트 설정

--- updown/client/src/components/Form.jsx
+++ updown/client/src/components/Form.jsx
@@ -4,7 +4,7 @@ import { Input } from "./Input";
 
 export const Form = ({ onSubmit, className, action }) => { 
   return (
-    <form className={className} onSubmit={onSubmit} action={action}>
+    <form className={className} onSubmit={onSubmit} action={action} method="post">
       <Input type="text" placeholder="아이디" />   
       <Input type="password" placeholder="비밀번호" />   
       <Button btnName="로그인" type="submit"></Button>
