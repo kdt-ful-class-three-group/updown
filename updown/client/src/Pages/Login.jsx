f1fc9d4ef5ad55b6491f3c6c6b22e5a26a5fd3c3
kimyohun <yohunkim0@gmail.com>
2025-04-23 14:31:24 +0900

    Login 페이지에서 DB의 user_info 테이블 데이터 fetch로 가져옴

--- updown/client/src/Pages/Login.jsx
+++ updown/client/src/Pages/Login.jsx
@@ -1,10 +1,27 @@
 import { Form, useNavigate } from "react-router-dom"
+import { useEffect } from "react";
 import { Button } from "../components/Button"
 import { Input } from "../components/Input";
 
 export const Login = () => {
 
   const navigate = useNavigate();
+  
+  const fetchGet = () => {
+    fetch("http://localhost:8003/", {
+      method: "GET",
+      headers: { 
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => console.log(data))
+      .catch((err) => console.log(`${err} 에러발생`));
+  }
+
+  useEffect(() => {
+    fetchGet();
+  }, [])
 
 return <>
         <Input placeholder="id" type="text"/>
--- updown/server/config/database.js
+++ updown/server/config/database.js
@@ -3,8 +3,8 @@ const mysql = require('mysql2/promise');
 const pool = mysql.createPool({
   host: 'localhost',
   user: 'root',
-  password: '',
-  database: '',
+  password: 'across1!@#',
+  database: 'test',
   port: 3306
 })
 
