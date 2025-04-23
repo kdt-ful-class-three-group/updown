a5ec8c2c116884d6c92670ab435f0026299f18e3
kimyohun <yohunkim0@gmail.com>
2025-04-23 15:29:55 +0900

    Login.jsx에 input value 감지

--- updown/client/src/Pages/Login.jsx
+++ updown/client/src/Pages/Login.jsx
@@ -1,10 +1,11 @@
 import { Form, useNavigate } from "react-router-dom"
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { Button } from "../components/Button"
 import { Input } from "../components/Input";
 
 export const Login = () => {
-
+  const [id, setIdValue] = useState('');
+  const [pw, setPwValue] = useState('');
   const navigate = useNavigate();
   
   const fetchGet = () => {
@@ -23,9 +24,14 @@ export const Login = () => {
     fetchGet();
   }, [])
 
+  console.log(id);
+  console.log(pw);
+
 return <>
-        <Input placeholder="id" type="text"/>
-        <Input placeholder="pw" type="text"/>
+        <input type="text" onChange={(e)=> (setIdValue(e.target.value))} />
+        <input type="password" onChange={(e)=>(setPwValue(e.target.value))}/>
+        {/* <Input placeholder="id" type="text"/>
+        <Input placeholder="pw" type="text"/> */}
         <Button btnName={"로그인"} onClick={() => {navigate('/game')}}/>
         <Button btnName={"회원가입"} onClick={() => {navigate('/SignUp')}}/>
        </>
