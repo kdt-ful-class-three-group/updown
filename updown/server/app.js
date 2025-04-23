86aeb5840919390cd1b10ad16422fc23762eccd7
최현준 <choijjunii1@gmail.com>
2025-04-23 10:27:53 +0900

    fetch 중 중단

--- updown/server/app.js
+++ updown/server/app.js
@@ -1,29 +1,18 @@
-const express = require('express');
-const router = express.Router();
+import express from 'express';
+import cors from 'cors';
+import bodyparser from 'body-parser';
+import router from './routes/user.js';
+
+
 const app = express();
 
 
+app.use(cors());
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({ extended: true }));
 
-app.use(express.json());
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+app.use('/users', router);
 
 app.listen(8003, () => {
   console.log('Server running on http://localhost:8003');
 });
-
-
-
