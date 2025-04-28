b626b08cf2a85d0179a4f105135b787551c6419a
kimyohun <yohunkim0@gmail.com>
2025-04-28 14:26:14 +0900

    필요한 유효성 검증 데이터 추가

--- updown/client/src/pages/SignUpValid.jsx
+++ updown/client/src/pages/SignUpValid.jsx
@@ -2,9 +2,13 @@ export const SignUpValid = ({ id, password, name, email }) => {
     // 원하는 유효성 검사 파일들 담아놓기
     const validation = [
       { condition: !id, message: "아이디를 입력해주세요" },
+      { condition: id.length < 4, message: "아이디는 4자 이상 입력해주세요" },
       { condition: !password, message: "비밀번호를 입력해주세요" },
+      { condition : password.length < 8 , message : "비밀번호는 8자 이상 입력이 필요합니다"},
       { condition: !name, message: "이름 입력해주세요" },
-      { condition: !email.includes("@"), message: "이메일 형식이 아닙니다." }
+      { condition : name.length < 4, message : "이름은 4자 이상 입력이 필요합니다"},
+      { condition: !email.includes("@"), message: "이메일 형식이 아닙니다" },
+      { condition : email.length < 5, message : "이메일은 5자 이상 입력해주세요"}
     ]
   
     // 각각의 유효성 검사 실시
