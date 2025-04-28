27fe95dbaf0c93acc235b7e0893a6ac6c0782cc1
kimyohun <yohunkim0@gmail.com>
2025-04-28 12:37:22 +0900

    예외처리 기본 샘플

--- updown/client/src/pages/SignUpPage.jsx
+++ updown/client/src/pages/SignUpPage.jsx
@@ -2,6 +2,27 @@ import React, { useState } from "react"
 import { useNavigate } from "react-router-dom"
 import { Button } from "../components/Button"
 
+const signup_valid = ({id, password, name, email}) => {
+  if (id === '') {
+    alert("아이디를 입력해주세요.");
+    return false;
+  }
+  if (password === '') {
+    alert("비밀번호를 입력해주세요.");
+    return false;
+  }
+  if (name === '') {
+    alert("이름을 입력해주세요.");
+    return false;
+  }
+  if (!email.includes("@")) {
+    alert("이메일 형식이 아닙니다.");
+    return false;
+  }
+  return true;
+};
+
+
 export const SignUpPage = () => {
   const [id, setId] = useState("");
   const [password, setPassword] = useState("");
@@ -9,48 +30,45 @@ export const SignUpPage = () => {
   const [email, setEmail] = useState("");
 
   const navigate = useNavigate();
-
-  const userData = {
-    id: id,
-    password: password,
-    name: name,
-    email: email
-  };
   
+  // 클릭하면 fetch 실행
   const onClickBtn = async () => {
-    if (id === "" || password === "" || name === "" || email === "") {
-      alert("모든 필드를 입력해주세요.");
-      return;
+
+    const valid = signup_valid({id, password, name, email});
+    if(valid){
+      try {
+        const res = await fetch("http://localhost:8003/signup", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({id : id, password : password, name : name, email : email}),
+        });
+  
+        const data = await res.json();
+        console.log(data);
+  
+        alert("가입이 완료되었습니다.");
+        navigate('/login');
+  
+      } catch (err) {
+        console.log(`${err} 에러발생`);
+      }      
     }
 
-    try {
-      const res = await fetch("http://localhost:8003/signup", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(userData),
-      });
-
-      const data = await res.json();
-      console.log(data);
-
-      alert("가입이 완료되었습니다.");
-      navigate('/login');
-
-    } catch (err) {
-      console.log(`${err} 에러발생`);
-    }
   };
 
-  return <>
+  return(
+  <>
     <h1>회원가입</h1>
     <div>
-    <input type="text" placeholder="아이디" value={id} onChange={(e)=> setId(e.target.value)}/>
-    <input type="password" placeholder="비밀번호" value={password} onChange={(e) => setPassword(e.target.value)} />
-    <input type="text" placeholder="이름" name="name" value={name} onChange={(e)=> setName(e.target.value)} />
-    <input type="text" placeholder="이메일" name="email" value={email} onChange={(e)=> setEmail(e.target.value)} />
+    <input type="text" placeholder="아이디"  onChange={(e)=> setId(e.target.value)}/>
+    <input type="password" placeholder="비밀번호"  onChange={(e) => setPassword(e.target.value)} />
+    <input type="text" placeholder="이름" name="name"  onChange={(e)=> setName(e.target.value)} />
+    <input type="text" placeholder="이메일" name="email" onChange={(e)=> setEmail(e.target.value)} />
     <Button btnName={"가입"} type="submit" onClick={onClickBtn} />
     </div>
-  </>
+  </> 
+  )
+
 }
--- updown/server/routes/signup.js
+++ updown/server/routes/signup.js
@@ -6,10 +6,11 @@ const router = express.Router();
 //* 회원가입
 router.post('/', async (req, res) => {
   const { id, password, name, email } = req.body;
+  console.log(password);
   try {
     // 데이터 삽입 INSERT INTO : 테이블에 새 행을 추가하는 명령어
     // 여기서 ?는 자리표시자로, 실제 값은 배열의 요소로 대체됨 (id, password, name, email)
-    await pool.query('INSERT INTO users (user_id, password, name, e_mail) VALUES (?,?,?,?)', [id, password, name, email]);
+    await pool.query('INSERT INTO user_info (id, password, name, email) VALUES (?,?,?,?)', [id, password, name, email]);
     res.status(201).json({id, password, name, email});
   } catch (err) {
     console.error(err.message);
