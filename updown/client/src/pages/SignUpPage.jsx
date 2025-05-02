44c80c97310b92df76b4f2cf1632b2a57b487779
kimyohun <yohunkim0@gmail.com>
2025-05-02 17:33:41 +0900

    아이디, 비밀번호, 이름, 이메일 유효성 검사 확인 및 확인 후 fetch 진행

--- updown/client/src/pages/SignUpPage.jsx
+++ updown/client/src/pages/SignUpPage.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react"
 import { useNavigate } from "react-router-dom"
 import { Button } from "../components/Button"
+import { SignUpValid } from "./SignValid"
 
 export const SignUpPage = () => {
   const [id, setId] = useState("");
@@ -10,47 +11,47 @@ export const SignUpPage = () => {
 
   const navigate = useNavigate();
 
-  const userData = {
-    id: id,
-    password: password,
-    name: name,
-    email: email
-  };
-  
   const onClickBtn = async () => {
-    if (id === "" || password === "" || name === "" || email === "") {
-      alert("모든 필드를 입력해주세요.");
-      return;
-    }
+    // if (id === "" || password === "" || name === "" || email === "") {
+    //   alert("모든 필드를 입력해주세요.");
+    //   return;
+    // }
 
-    try {
-      const res = await fetch("http://localhost:8003/signup", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(userData),
-      });
+    // 유효성검사가 true면 실행
+    const validPass = SignUpValid({id, password, name, email})
 
-      const data = await res.json();
-      console.log(data);
+    // 유효성 통과가 확인되면 실행
+    if (validPass) {
+      try {
+        const res = await fetch("http://localhost:8003/signup", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({id : id, password : password, name : name, email : email}),
+        });
 
-      alert("가입이 완료되었습니다.");
-      navigate('/login');
+        const data = await res.json();
+        console.log(data);
 
-    } catch (err) {
-      console.log(`${err} 에러발생`);
-    }
-  };
+        alert("가입이 완료되었습니다.");
+        navigate('/login');
+
+      } catch (err) {
+        console.log(`${err} 에러발생`);
+      }
+    };
+
+  }
 
   return <>
     <h1>회원가입</h1>
     <div>
-    <input type="text" placeholder="아이디" value={id} onChange={(e)=> setId(e.target.value)}/>
-    <input type="password" placeholder="비밀번호" value={password} onChange={(e) => setPassword(e.target.value)} />
-    <input type="text" placeholder="이름" name="name" value={name} onChange={(e)=> setName(e.target.value)} />
-    <input type="text" placeholder="이메일" name="email" value={email} onChange={(e)=> setEmail(e.target.value)} />
-    <Button btnName={"가입"} type="submit" onClick={onClickBtn} />
+      <input type="text" placeholder="아이디" value={id} onChange={(e) => setId(e.target.value)} />
+      <input type="password" placeholder="비밀번호" value={password} onChange={(e) => setPassword(e.target.value)} />
+      <input type="text" placeholder="이름" name="name" value={name} onChange={(e) => setName(e.target.value)} />
+      <input type="text" placeholder="이메일" name="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+      <Button btnName={"가입"} type="submit" onClick={onClickBtn} />
     </div>
   </>
 }
---
+++ updown/client/src/pages/SignValid.jsx
@@ -0,0 +1,25 @@
+export const SignUpValid = ({ id, password, name, email }) => {
+    // 원하는 유효성 검사 파일들 담아놓기
+    const validation = [
+      { condition: !id, message: "아이디를 입력해주세요" },
+      { condition: id.length < 4, message: "아이디는 4자 이상 입력해주세요" },
+      { condition: !password, message: "비밀번호를 입력해주세요" },
+      { condition : password.length < 8 , message : "비밀번호는 8자 이상 입력이 필요합니다"},
+      { condition: !name, message: "이름 입력해주세요" },
+      { condition : name.length < 4, message : "이름은 4자 이상 입력이 필요합니다"},
+      { condition: !email.includes("@"), message: "이메일 형식이 아닙니다" },
+      { condition : email.length < 5, message : "이메일은 5자 이상 입력해주세요"}
+    ]
+  
+    // 각각의 유효성 검사 실시
+    for (let validate of validation) {
+      // 각각의 값들이 true면 alert창 실행
+      if (validate.condition) {
+        // 각 유효성에 맞게 메세지를 표출
+        alert(validate.message)
+        return false
+      }
+    }
+    // 문제가 없을 시 true로 진행
+    return true;
+  };
