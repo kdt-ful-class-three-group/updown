4d669f70b47177702d27c572ef5be62e99751dfd
전선일 <jeonsunil1225@gamil.com>
2025-05-02 15:38:48 +0900

    sort()를 활용할 때, a와 b를 인식할 때, a와 b가 문자열일때, 숫자열일 때, 기능하는 부분이 달라서, 문제가 생긴 부분을 해결하고, 주석을 달아놓음.

--- updown/client/src/pages/RankingPage.jsx
+++ updown/client/src/pages/RankingPage.jsx
@@ -35,23 +35,33 @@ export const RankingPage = () => {
     mode : item.mode,
     success : item.success,
     total : item.total,
-    rate : Math.round((item.success / item.total) * 100) + '%'
+    // * rate를 %를 추가해서 데이터를 가공하는 경우, -> %는 유니코드로 문자열로 취급되어 js에서 숫자열 + '%' 를 넣으면 문자열 '숫자%' 로 변환된다.
+    // rate : Math.round((item.success / item.total) * 100) + %
+    // * 문자열이 아닌 숫자열로써 사용하기 위해서 + %를 지우고, 사용한다.
+    rate : Math.round((item.success / item.total) * 100)
   }))
 
   console.log(sortedData);
   // 확률 기준으로 정렬
   sortedData.sort(function (a, b) {
-    if (a.rate > b.rate) {
-      return 1;
-    }
-    if (a.rate < b.rate) {
-      return -1;
-    }
-    // a must be equal to b
-    return 0;
+  
+    // * >, <, = 의 형태의 부호들은 true, false 값으로 판단해서, '숫자%'를 이해할 수 있다. 
+    // if (a.rate > b.rate) {
+    //   return 1;
+    // }
+    // if (a.rate < b.rate) {
+    //   return -1;
+    // }
+    // // a must be equal to b
+    // return 0;
+    // * 그러나 b - a, b + a 와 같은 형태로 사용하는 경우에는 숫자열만을 인식한다. 
+    // * 만일 위에서 rate에 %를 '+ %' 로 추가한 경우 .split으로 '%'앞의 것만 불러와서, 문자열 형태로 사용한다.
+    // retrun b.rate.split('%')[0] - a.rate.split('%')[0] 
+    return b.rate - a.rate;
   });
 
   console.log(sortedData);
+  // * 위에서 + %를 지웠으므로, 표출 할 때, %를 추가해준다. {item.rat}로만 되어있던 부분을 {item.rate}{'%'} 로 변경해 준다.
   return (
     <div>
       <h1>랭킹</h1>
@@ -80,7 +90,7 @@ export const RankingPage = () => {
                 <td>{item.mode}</td>
                 <td>{item.success}</td>
                 <td>{item.total}</td>
-                <td>{item.rate}</td>
+                <td>{item.rate}{'%'}</td>
               </tr>
             ))}
           </tbody>
