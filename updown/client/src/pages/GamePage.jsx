18d22cdb44c537becc39e8b641458c4084cb4a19
최현준 <choijjunii1@gmail.com>
2025-05-09 15:57:02 +0900

    history-box제외 마무리

--- updown/client/src/pages/GamePage.jsx
+++ updown/client/src/pages/GamePage.jsx
@@ -1,92 +1,93 @@
 // 리액트, 훅
-import React, { useMemo, useRef, useState, useEffect } from 'react';
-import { useParams, useNavigate } from 'react-router-dom';
-import { useMessage } from '../context/MessageContext';
+import React, { useMemo, useRef, useState, useEffect } from "react";
+import { useParams, useNavigate } from "react-router-dom";
+import { useMessage } from "../context/MessageContext";
 
 // 레벨별 설정값.
-import { levelData } from '../config/levelData';
+import { levelData } from "../config/levelData";
 
 // 정답 맞추는 로직 함수
-import { InputNum } from '../utils/InputNum';
+import { InputNum } from "../utils/InputNum";
 
 // 태그 컴포넌트들
-import { Div, Heading } from '../components/Tag';
-import { Button } from '../components/Button';
-import { Input } from '../components/Input';
+import { Div, Heading } from "../components/Tag";
+import { Button } from "../components/Button";
+import { Input } from "../components/Input";
 
 //*  이지,노말,하드 모두 한 템플릿(GamePage)에서 작동
 
 export function GamePage() {
   // useParams를 통해 url에서 level(easy, normal, hard) 가져오기
-  if (sessionStorage.getItem('id')) {
-    const { level } = useParams();
+  const { level } = useParams();
 
-    const navigate = useNavigate();
-    // useState를 통해 input값을 관리
-    const [inputValue, setInputValue] = useState('');
-    // useMessage를 통해 message값을 관리
-    // useMessage는 MessageContext에서 만든 커스텀 훅
-    const { message, setMessage } = useMessage();
-    // useMemo를 통해 level에 따라 maxNum을 설정
+  const navigate = useNavigate();
+  // useState를 통해 input값을 관리
+  const [inputValue, setInputValue] = useState("");
+  // useMessage를 통해 message값을 관리
+  // useMessage는 MessageContext에서 만든 커스텀 훅
+  const { message, setMessage } = useMessage();
+  // useMemo를 통해 level에 따라 maxNum을 설정
 
-    // levelData에서 level에 해당하는 설정값을 가져옴
-    const setting = useMemo(() => levelData[level], [level]);
-    const maxNum = setting.max;
-    const [count, setCount] = useState(setting.count);
-    // * history와 status는 바뀔 값들이기에 useState를 통해 관리
-    const [history, setHistory] = useState([]);
-    const [status, setStatus] = useState(null);
+  // levelData에서 level에 해당하는 설정값을 가져옴
+  const setting = useMemo(() => levelData[level], [level]);
+  const maxNum = setting.max;
+  const [count, setCount] = useState(setting.count);
+  // * history와 status는 바뀔 값들이기에 useState를 통해 관리
+  const [history, setHistory] = useState([]);
+  const [status, setStatus] = useState(null);
 
+  //! randomNum을 한 번만 초기화
+  const randomNum = useRef(null);
+  // useEffect를 통해 randomNum을 초기화
+  useEffect(() => {
+    // 시작할 때 콘솔이 두번찍히는 이유는 main.jsx에  <StrictMode>가 있기 때문. 두번 렌더링되서 그런것
+    // StrictMode를 제거하면 한 번만 렌더링됨 or if (randomNum.current === null) 조건을 추가해서 한 번만 렌더링되게 할 수 있음
+    if (randomNum.current === null) {
+      randomNum.current = Math.floor(Math.random() * maxNum) + 1;
+      console.log(`Game Start! - ${level} mode`);
+      console.log(randomNum.current);
+      // 중복 쿼리스트링 방지를 위한 조건
+    }
+  }, [maxNum]);
 
-    //! randomNum을 한 번만 초기화
-    const randomNum = useRef(null);
-    // useEffect를 통해 randomNum을 초기화
-    useEffect(() => {
-      // 시작할 때 콘솔이 두번찍히는 이유는 main.jsx에  <StrictMode>가 있기 때문. 두번 렌더링되서 그런것
-      // StrictMode를 제거하면 한 번만 렌더링됨 or if (randomNum.current === null) 조건을 추가해서 한 번만 렌더링되게 할 수 있음
-      if (randomNum.current === null) {
-        randomNum.current = Math.floor(Math.random() * maxNum) + 1;
-        console.log(`Game Start! - ${level} mode`);
-        console.log(randomNum.current);
-        // 중복 쿼리스트링 방지를 위한 조건
-      }
-    }, [maxNum]);
+  // 다시하기를 했을 때 up,down 메세지가 그대로 남아있어 useEffect를 사용하여 message를 초기화
+  useEffect(() => {
+    setMessage("");
+  }, []);
 
-    // 다시하기를 했을 때 up,down 메세지가 그대로 남아있어 useEffect를 사용하여 message를 초기화
-    useEffect(() => {
-      setMessage('');
-    }, [])
+  // *useEffect를 사용하여, status값이 변경되면, navigate를 통해 결과 페이지로 이동
+  // * status는 success, fail로 나뉨 => InputNum에 status를 변경하는 코드가 있음.
+  useEffect(() => {
+    if (status !== null) {
+      // 버튼을 누르면 게임을 플레이 한 것으로 판단하여 true로 세션 저장
+      sessionStorage.setItem("passed", true);
 
-    // *useEffect를 사용하여, status값이 변경되면, navigate를 통해 결과 페이지로 이동
-    // * status는 success, fail로 나뉨 => InputNum에 status를 변경하는 코드가 있음.
-    useEffect(() => {
-      if (status !== null) {
-        // 버튼을 누르면 게임을 플레이 한 것으로 판단하여 true로 세션 저장
-        sessionStorage.setItem('passed', true)
-      
-        // * 들어오는 status에 따라 header에 성공, 실패가 결정되고, history의 데이터를 통해, 이전에 입력한 숫자에 대한 내용이 결정 된다.
-        // * navigate를 GamePage에서 관리를 하는 이유는 InputNum에서는 status와 history를 InputNum 함수의 매개 변수로 받아오는데, 그러면 useEffect를 사용해 status와 history를 관리 할 수 없다. 그리고, useEffect를 사용해야 하는 이유는, status와 history가 바뀐 이후에 navigate를 해야 하는데, useEffect를 사용하지 않으면, navigate가 먼저 실행되고, history가 바뀌지 않고 넘어가 마지막으로 입력한 history가 추가 되지 않기 때문. 
-        navigate(`/result?status=${status}&history=${history}&level=${level}`);
-      }
-    }, [status, history])
-  
+      // * 들어오는 status에 따라 header에 성공, 실패가 결정되고, history의 데이터를 통해, 이전에 입력한 숫자에 대한 내용이 결정 된다.
+      // * navigate를 GamePage에서 관리를 하는 이유는 InputNum에서는 status와 history를 InputNum 함수의 매개 변수로 받아오는데, 그러면 useEffect를 사용해 status와 history를 관리 할 수 없다. 그리고, useEffect를 사용해야 하는 이유는, status와 history가 바뀐 이후에 navigate를 해야 하는데, useEffect를 사용하지 않으면, navigate가 먼저 실행되고, history가 바뀌지 않고 넘어가 마지막으로 입력한 history가 추가 되지 않기 때문.
+      navigate(`/result?status=${status}&history=${history}&level=${level}`);
+    }
+  }, [status, history]);
 
-    // inputHandler는 input값을 관리하는 함수
-    const inputHandler = (e) => {
-      setInputValue(e.target.value);
-    };
+  // inputHandler는 input값을 관리하는 함수
+  const inputHandler = (e) => {
+    setInputValue(e.target.value);
+  };
 
-    return (
-      <Div>
-        <Div>
-          <Heading number={1} content={level} />
-        </Div>
-        <Div>
-          {/* UP,DOWN 메세지가 들어갈 컨테이너  */}
-          <Heading number={1} content={message} />
-          <Div className={count === 1 ? "color-red" : ""} children={`남은기회 : ${count}`} />
-        </Div>
-        <Div>
+  return (
+    <Div className="container">
+      <Div className="mode-title">
+        <h1>{level}</h1>
+      </Div>
+      <Div className="updown-msg">
+        {/* UP,DOWN 메세지가 들어갈 컨테이너  */}
+        <h1>{message}</h1>
+      </Div>
+      <Div className="game-box">
+        <Div
+          className={count === 1 ? "color-red" : ""}
+          children={`남은기회 : ${count}`}
+        />
+        <div className="guess-form">
           <Input value={inputValue} onChange={inputHandler} />
           <Button
             btnName="확인"
@@ -100,22 +101,25 @@ export function GamePage() {
                 setCount,
                 history,
                 setHistory,
-                setStatus
+                setStatus,
               })
             }
           />
-        </Div>
-        <Div>
-          <p>이전에 입력한 숫자</p>
-          <p>{history[0]}</p>
-          <p>{history[1]}</p>
-          <p>{history[2]}</p>
-          <p>{history[3]}</p>
-        </Div>
-        <div>
-          <button className='all-btn' onClick={() => navigate('/mode')}>처음으로</button>
         </div>
       </Div>
-    );
-  }
+      <Div className="history-box">
+        <p>이전에 입력한 숫자</p>
+        <p>{history[0]}</p>
+        <p>{history[1]}</p>
+        <p>{history[2]}</p>
+        <p>{history[3]}</p>
+      </Div>
+
+      <div className="start-btn">
+        <button className="all-btn" onClick={() => navigate("/mode")}>
+          처음으로
+        </button>
+      </div>
+    </Div>
+  );
 }
--- updown/client/src/pages/ResultPage.jsx
+++ updown/client/src/pages/ResultPage.jsx
@@ -1,10 +1,9 @@
-import React from 'react';
-import { useEffect, useRef } from 'react';
-import { data, useLocation,useNavigate } from 'react-router-dom';
-import { Button } from '../components/Button';
-import { Div, Heading } from '../components/Tag';
+import React from "react";
+import { useEffect, useRef } from "react";
+import { data, useLocation, useNavigate } from "react-router-dom";
+import { Button } from "../components/Button";
+import { Div, Heading } from "../components/Tag";
 // import { setEmitFlags } from 'typescript';
-
 
 //* ResultPage 컴포넌트 - 결과 페이지
 //* 성공, 실패 결과에 따라 페이지 이동할 예정 - 지금은 성공일 때만 작동
@@ -19,124 +18,140 @@ export function ResultPage() {
   const queryParams = new URLSearchParams(location.search);
 
   // * url에서 status의 값을 가져옴. status는 success, fail로 나뉨
-  const status = queryParams.get('status'); 
+  const status = queryParams.get("status");
 
   // * url에서 history의 값을 가져옴. history는 이전에 입력한 숫자들
   // * history는 문자열로 되어있으므로 split(',')을 사용하여 ,기준으로 나누어 배열로 변환 이후, map(Number)로 숫자로 변환.
-  const history = queryParams.get('history').split(',').map(Number); // 문자열을 숫자로 변환 
+  const history = queryParams.get("history").split(",").map(Number); // 문자열을 숫자로 변환
 
-
-  const successOrFail = status === 'success';
+  const successOrFail = status === "success";
   // * 연속해서 사용되는 button요소들을 묶어서 변수에 담아줌
-  const moveEvent = 
+  const moveEvent = (
     <>
-      <Button btnName={'다시하기'} onClick={() => { sessionStorage.removeItem('data'); navigate(-1); }} />
-      <Button btnName={'모드선택'} onClick={() => { sessionStorage.removeItem('data'); navigate(-2); }} />
-      <Button btnName={'홈'} onClick={() => {sessionStorage.removeItem('data'); navigate(-3);}} />
-    </>;   
+      <Button
+        className="all-btn"
+        btnName={"다시하기"}
+        onClick={() => {
+          sessionStorage.removeItem("data");
+          navigate(-1);
+        }}
+      />
+      <Button
+        className="all-btn"
+        btnName={"모드선택"}
+        onClick={() => {
+          sessionStorage.removeItem("data");
+          navigate(-2);
+        }}
+      />
+      <Button
+        className="all-btn"
+        btnName={"홈"}
+        onClick={() => {
+          sessionStorage.removeItem("data");
+          navigate(-3);
+        }}
+      />
+    </>
+  );
 
-  
   // * 연속해서 사용되는 history요소들을 묶어서 변수에 담아줌
-  // * history는 최대 4개까지만 생성 되므로, 이후 스타일을 넣을 때, 모든 칸에 underline을 만들어 주기 위해, 밀 p태그를 생성해둠, 
-  const historyBox = 
-  <>
-    <Div>
-      <p>이전에 입력한 숫자</p>
-      <p>{history[0]}</p>
-      <p>{history[1]}</p>
-      <p>{history[2]}</p>
-      <p>{history[3]}</p>
-    </Div>
-  </>;
+  // * history는 최대 4개까지만 생성 되므로, 이후 스타일을 넣을 때, 모든 칸에 underline을 만들어 주기 위해, 밀 p태그를 생성해둠,
+  const historyBox = (
+    <>
+      <Div>
+        <p>이전에 입력한 숫자</p>
+        <p>{history[0]}</p>
+        <p>{history[1]}</p>
+        <p>{history[2]}</p>
+        <p>{history[3]}</p>
+      </Div>
+    </>
+  );
 
   // * useEffect를 사용해서 ResultPage가 렌더링 될 때 DB에 저장.
   useEffect(() => {
-    if(!sessionStorage.getItem('id') || !sessionStorage.getItem('passed')) {
+    if (!sessionStorage.getItem("id") || !sessionStorage.getItem("passed")) {
       // 세션이 없는 상태에서 접근하면 오류를 발생
-      console.log('유효하지 않는 접근입니다.')
-      sessionStorage.removeItem('passed');
-      sessionStorage.removeItem('data');
-      navigate('/mode');
-
+      console.log("유효하지 않는 접근입니다.");
+      sessionStorage.removeItem("passed");
+      sessionStorage.removeItem("data");
+      navigate("/mode");
     } else {
-    if (recordRef.current) return;
-    recordRef.current = true;
+      if (recordRef.current) return;
+      recordRef.current = true;
 
-    const user_id = sessionStorage.getItem('id');
-    const mode = queryParams.get('level');
-    const key = 'data';
-    
-    const gameRecordSave = sessionStorage.getItem(key);
-    if (gameRecordSave) {
-      sessionStorage.removeItem('passed');
-      sessionStorage.removeItem('data');
-      navigate('/mode');
-      console.log('이미 저장함');
-      return;
-    }
+      const user_id = sessionStorage.getItem("id");
+      const mode = queryParams.get("level");
+      const key = "data";
 
-    const gameRecord = {
-      user_id, // 세션에서 가져온 아이디
-      mode, // url에서 가져옴
-      success: successOrFail ? 1 : 0,// 성공여부
-    };
-    console.log(gameRecord);
+      const gameRecordSave = sessionStorage.getItem(key);
+      if (gameRecordSave) {
+        sessionStorage.removeItem("passed");
+        sessionStorage.removeItem("data");
+        navigate("/mode");
+        console.log("이미 저장함");
+        return;
+      }
 
-    sessionStorage.setItem(key, JSON.stringify(gameRecord));
+      const gameRecord = {
+        user_id, // 세션에서 가져온 아이디
+        mode, // url에서 가져옴
+        success: successOrFail ? 1 : 0, // 성공여부
+      };
+      console.log(gameRecord);
 
-    const validPass = sessionStorage.getItem('passed')
+      sessionStorage.setItem(key, JSON.stringify(gameRecord));
 
-    if(validPass){
-      fetch('http://localhost:8003/record', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(gameRecord),
+      const validPass = sessionStorage.getItem("passed");
 
-      })
-        .then((response) => {
+      if (validPass) {
+        fetch("http://localhost:8003/record", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(gameRecord),
+        }).then((response) => {
           if (response.ok) {
-            console.log('기록 저장되었습니다.');
+            console.log("기록 저장되었습니다.");
             // alert('기록 저장되었습니다.');
           } else {
-            console.error('기록 저장 실패');
-            alert('기록 저장 실패');
+            console.error("기록 저장 실패");
+            alert("기록 저장 실패");
           }
         });
+      }
     }
-  }
   }, []);
-  
-  
 
-// * Div의 Children으로 moveEvent와 historyBox를 넣어줌
-// * successOrFail이 true일 때와 false일 때를 나누어 줌
-// * successOrFail이 true일 때는 닉네임을 입력받는 버튼을 추가로 보여줌
+  // * Div의 Children으로 moveEvent와 historyBox를 넣어줌
+  // * successOrFail이 true일 때와 false일 때를 나누어 줌
+  // * successOrFail이 true일 때는 닉네임을 입력받는 버튼을 추가로 보여줌
   return (
-      <Div>
-      <Heading number={1} content={successOrFail ? '성공' : '실패'} />
-      
-        <Div children={successOrFail ? 
-          <>
-          {moveEvent}
+    <Div>
+      <Heading number={1} content={successOrFail ? "성공" : "실패"} />
 
-          
-          {historyBox}
+      <Div
+        children={
+          successOrFail ? (
+            <>
+              {moveEvent}
 
-          </>
-          
-          : 
-          <>
-          {moveEvent}
-          {historyBox}
-          </>
-          } />
-          {/* 페이지 이동 버튼들 */}
-          {/* navigate 경로를 '/' 직접설정할 수 있지만 -숫자 이렇게 쓰면 숫자만큼 뒤로 갈 수 있음.
+              {historyBox}
+            </>
+          ) : (
+            <>
+              {moveEvent}
+              {historyBox}
+            </>
+          )
+        }
+      />
+      {/* 페이지 이동 버튼들 */}
+      {/* navigate 경로를 '/' 직접설정할 수 있지만 -숫자 이렇게 쓰면 숫자만큼 뒤로 갈 수 있음.
           서로의 장단점은 아직 모르겠음 */}
-
-        </Div>
-      // </Div>
-  )
+    </Div>
+    // </Div>
+  );
 }
