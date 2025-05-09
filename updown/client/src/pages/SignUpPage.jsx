b5b096a930df64163dfa44e01cc6e4ea651db310
전선일 <jeonsunil1225@gamil.com>
2025-05-09 11:30:16 +0900

    useMessage 컴포넌트를 활용하여 표출

--- updown/client/src/pages/SignUpPage.jsx
+++ updown/client/src/pages/SignUpPage.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react"
 import { useNavigate } from "react-router-dom"
 import { Button } from "../components/Button"
 import { SignUpValid } from "./SignValid"
+import { useMessage } from "../context/MessageContext"
 
 export const SignUpPage = () => {
   const [id, setId] = useState("");
@@ -10,6 +11,7 @@ export const SignUpPage = () => {
   const [email, setEmail] = useState("");
   const [validID, setValidID] = useState(null);
   const [validName, setVaildName] = useState(null);
+  const {message, setMessage} = useMessage();
   const navigate = useNavigate();
   
   const getFetch = async () => {
@@ -32,6 +34,8 @@ export const SignUpPage = () => {
       console.log(`${err} 에러발생`);
     }
   };
+
+
 
 
   const onClickBtn = async () => {
@@ -63,19 +67,45 @@ export const SignUpPage = () => {
 
   }
 
+  useEffect(() => {
+    console.log(validID);
+    if(validID === true) {
+      setMessage('사용중인 아이디 입니다.');
+    } else if(validID === false) {
+      setMessage('사용가능한 아이디 입니다.');
+    } else {
+      setMessage('');
+    }
+  }, [validID]);
+
+  useEffect(() => {
+    console.log(validName);
+    if(validName === true) {
+      setMessage('사용중인 닉네임 입니다.');
+    } else if(validName === false) {
+      setMessage('사용가능한 닉네임 입니다.');
+    } else {
+      setMessage('');
+    }
+  }, [validName]);
+
+      // {/* 아이디가 중복되면 p태그로 출력 */}
   return <>
     <h1>회원가입</h1>
-    <div>
+    <div className="signup">
       <input type="text" placeholder="아이디" value={id} onChange={(e) => setId(e.target.value)} />
-      {/* 아이디가 중복되면 p태그로 출력 */}
-      {validID ? <p>중복된 아이디입니다.</p> : <p>사용가능한 ID 입니다.</p>}
+      {message === '사용중인 아이디 입니다.' ? message : ''}
+      {message === '사용가능한 아이디 입니다.' ? message : ''}
       <button onClick={getFetch}>id 중복 확인</button>
       <input type="password" placeholder="비밀번호" value={password} onChange={(e) => setPassword(e.target.value)} />
       <input type="text" placeholder="이름" name="name" value={name} onChange={(e) => setName(e.target.value)} />
-      {validName ? <p>중복된 닉네임 입니다.</p> : <p>사용가능한 닉네임 입니다.</p>}
+      {message === '사용중인 닉네임 입니다.'  ? message : ''}
+      {message === '사용가능한 닉네임 입니다.'  ? message : ''}
       <button onClick={getFetch}>닉네임 중복 확인</button>
       <input type="text" placeholder="이메일" name="email" value={email} onChange={(e) => setEmail(e.target.value)} />
       <Button btnName={"가입"} type="submit" onClick={onClickBtn} />
     </div>
   </>
 }
+// {/* {validID ? <p>중복된 아이디입니다.</p> : <p>사용가능한 ID 입니다.</p>} */}
+// {/* {validName ? <p>중복된 닉네임 입니다.</p> : <p>사용가능한 닉네임 입니다.</p>} */}
