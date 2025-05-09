bc8fb28dc9aac3d933dee7070c22896e3939b597
egoing <shower0044@naver.com>
2025-05-09 15:22:46 +0900

    이미지 삽입

--- updown/client/src/pages/ModePage.jsx
+++ updown/client/src/pages/ModePage.jsx
@@ -5,19 +5,32 @@ import { Div } from "../components/Tag";
 
 //* ModePage 컴포넌트 - 모드 선택 페이지
 //* EASY, NORMAL, HARD 모드 선택 후 각 페이지로 이동
- export function ModePage() {
+export function ModePage() {
   // useNavigate 훅을 사용하여 페이지 이동
-   const navigate = useNavigate();
+  const navigate = useNavigate();
   return (
     <Div className="main-content">
-      <Div >
-      <h1>모드선택</h1>
+      <Div>
+        <img src="/vite.svg" className="start-logo" />
+        <h1>모드선택</h1>
       </Div>
       <Div>
-        <Button className="all-btn easy" btnName={'EASY'} onClick={() => navigate('/easy')}/>
-        <Button className="all-btn normal" btnName={'NORMAL'} onClick={() => navigate('/normal')}/>
-        <Button className="all-btn hard" btnName={'HARD'} onClick={() => navigate('/hard')}/>
+        <Button
+          className="all-btn easy"
+          btnName={"EASY"}
+          onClick={() => navigate("/easy")}
+        />
+        <Button
+          className="all-btn normal"
+          btnName={"NORMAL"}
+          onClick={() => navigate("/normal")}
+        />
+        <Button
+          className="all-btn hard"
+          btnName={"HARD"}
+          onClick={() => navigate("/hard")}
+        />
       </Div>
     </Div>
-  )
+  );
 }
